a8d9fe32e97b8d5ebb1379f958c460afe3c238b5
ralexrivero <ralexrivero@gmail.com>
2023-05-20 17:27:43 -0300

    fix twitter og

--- src/components/meta.jsx
+++ src/components/meta.jsx
@@ -12,11 +12,12 @@ const Meta = () => {
       <meta property="og:image" content={og_fb_profile} />
       <meta property="og:url" content="https://ralexrivero.github.io/ProBusinessCard/" />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://ralexrivero.github.io/ProBusinessCard/" />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content="Ronald Rivero - Desarrollador de software profesional y profesor de programación" />
-      <meta property="og:description" content="¡Hola! Soy Ronald Rivero, un desarrollador de software profesional y apasionado profesor de programación. Mi objetivo es crear soluciones innovadoras y brindar conocimientos a estudiantes en el mundo de la programación." />
-      <meta property="og:image" content={og_tw_profile} />
+
+      <meta name="twitter:title" content="Ronald Rivero - Desarrollador de software profesional y profesor de programación" />
+      <meta name="twitter:description" content="¡Hola! Soy Ronald Rivero, un desarrollador de software profesional y apasionado profesor de programación. Mi objetivo es crear soluciones innovadoras y brindar conocimientos a estudiantes en el mundo de la programación." />
+      <meta name="twitter:image" content={og_tw_profile} />
+      <meta name="twitter:card" content={og_tw_profile} />
+
     </Helmet>
   )
 }
