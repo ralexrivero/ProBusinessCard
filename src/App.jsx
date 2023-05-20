f027611946feb35c8ea2c4c7b03e88cff8a92764
ralexrivero <ralexrivero@gmail.com>
2023-05-20 17:07:40 -0300

    implement Helmet for meta

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Info from './components/info';
 import About from './components/about';
 import Interests from './components/interests';
 import Footer from './components/footer';
-
+import Meta from './components/meta';
 
 function App() {
   return (
     <div className="App container container-mb-s pt-xs">
       <div className="grd-col-1">
+        <Meta />
         <Info />
         <About />
         <Interests />
@@ -18,4 +19,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
---
+++ src/components/meta.jsx
@@ -0,0 +1,24 @@
+import { Helmet } from 'react-helmet-async';
+
+import og_fb_profile from '/img/og_fb_profile.jpg';
+import og_tw_profile from '/img/og_tw_profile.jpg';
+
+
+const Meta = () => {
+  return (
+    <Helmet>
+      <meta property="og:title" content="Ronald Rivero - Desarrollador de software profesional y profesor de programación" />
+      <meta property="og:description" content="¡Hola! Soy Ronald Rivero, un desarrollador de software profesional y apasionado profesor de programación. Mi objetivo es crear soluciones innovadoras y brindar conocimientos a estudiantes en el mundo de la programación." />
+      <meta property="og:image" content={og_fb_profile} />
+      <meta property="og:url" content="https://ralexrivero.github.io/ProBusinessCard/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://ralexrivero.github.io/ProBusinessCard/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Ronald Rivero - Desarrollador de software profesional y profesor de programación" />
+      <meta property="og:description" content="¡Hola! Soy Ronald Rivero, un desarrollador de software profesional y apasionado profesor de programación. Mi objetivo es crear soluciones innovadoras y brindar conocimientos a estudiantes en el mundo de la programación." />
+      <meta property="og:image" content={og_tw_profile} />
+    </Helmet>
+  )
+}
+
+export default Meta;
