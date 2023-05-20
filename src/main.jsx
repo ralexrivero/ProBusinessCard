11913b8cc96aed2c670bdddaa6ad4b1fe5981d73
ralexrivero <ralexrivero@gmail.com>
2023-05-20 17:07:40 -0300

    implement Helmet for meta

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,17 @@ import '../src/aquarius.css';
 import App from './App'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { I18nextProvider } from 'react-i18next';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+
 
 import i18next from './components/i18n';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18next}>
-      <App />
-    </I18nextProvider>
+    <HelmetProvider>
+      <I18nextProvider i18n={i18next}>
+        <App />
+      </I18nextProvider>
+    </HelmetProvider>
   </React.StrictMode>,
 )
