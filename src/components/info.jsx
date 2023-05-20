6beac8235c529bcb1d67dfe17b77fdb2c9b861c2
ralexrivero <ralexrivero@gmail.com>
2023-05-20 15:02:55 -0300

    fix href info attributes

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -19,9 +19,9 @@ const Info = () => {
         <span className="txt-c personal-title">{t('info.title')}</span>
         <small><a href="https://ralexrivero.github.io/" target="_blank" className="link link-website">{t('info.website')}</a></small>
         <div className="f-rsec">
-        <a href="mailto.ralexrivero@gmail.com" className="info-btn btn-email" target="_blank"><i className="bi bi-envelope"></i>{t('info.email')}</a>
+        <a href="mailto:ralexrivero@gmail.com" className="info-btn btn-email"><i className="bi bi-envelope"></i>{t('info.email')}</a>
         <a href="https://www.linkedin.com/in/ronald-rivero/" className="info-btn btn-linkedin" target="_blank"><i className="bi bi-linkedin"></i>{t('info.linkedin')}</a>
-        <a aria-label={t('info.whatsapp')} href="https.//wa.me/59893776930" className="info-btn btn-whatsapp" target="_blank"><i className="bi bi-whatsapp"></i></a>
+        <a aria-label={t('info.whatsapp')} href="https://wa.me/+59893776930" className="info-btn btn-whatsapp" target="_blank"><i className="bi bi-whatsapp"></i></a>
         </div>
       </div>
     </header>
