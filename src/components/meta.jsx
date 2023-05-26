6d1a9d62e7db402f3affe6727c14e0ef4172a029
ralexrivero <ralexrivero@gmail.com>
2023-05-25 23:34:49 -0300

    to avoid CORS use relative path

--- src/components/meta.jsx
+++ src/components/meta.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from 'react-helmet-async';
 
-import og_fb_profile from 'https://ralex.works/ProBusinessCard/img/og_fb_profile.jpg';
-import og_tw_profile from 'https://ralex.works/ProBusinessCard//img/og_tw_profile.jpg';
+import og_fb_profile from '../../img/og_fb_profile.jpg';
+import og_tw_profile from '../../img/og_tw_profile.jpg';
 
 
 const Meta = () => {
@@ -10,7 +10,7 @@ const Meta = () => {
       <meta property="og:title" content="Ronald Rivero - Desarrollador de software profesional y profesor de programación" />
       <meta property="og:description" content="¡Hola! Soy Ronald Rivero, un desarrollador de software profesional y apasionado profesor de programación. Mi objetivo es crear soluciones innovadoras y brindar conocimientos a estudiantes en el mundo de la programación." />
       <meta property="og:image" content={og_fb_profile} />
-      <meta property="og:url" content="https://ralex.works/ProBusinessCard/" />
+      <meta property="og:url" content="https://ralexrivero.github.io/ProBusinessCard/" />
       <meta property="og:type" content="website" />
 
       <meta name="twitter:title" content="Ronald Rivero - Desarrollador de software profesional y profesor de programación" />
