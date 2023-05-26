517f38570f093def156687fd5d69b2bc42a90f76
ralexrivero <ralexrivero@gmail.com>
2023-05-26 15:10:54 -0300

    add state and effect to toggle dark and light mode

--- src/components/theme.jsx
+++ src/components/theme.jsx
@@ -1,10 +1,24 @@
+import React, { useState, useEffect } from 'react';
+
 const Theme = () => {
+  const [lightMode, setLightMode] = useState(false);
+
+  const themeToggler = () => {
+    setLightMode(!lightMode);
+  }
+
+  useEffect(() => {
+    const body = document.querySelector('body');
+    body.classList.toggle('dark-theme', !lightMode);
+  }, [lightMode]);
+
   return (
     <div className="theme">
-      <i className="theme-btn bi bi-brightness-high "></i>
-      <i className="theme-btn bi bi-moon theme-btn-active"></i>
+      <i onClick={themeToggler} className="theme-btn bi bi-brightness-high"></i>
+      <i onClick={themeToggler} className="theme-btn bi bi-moon"></i>
     </div>
   );
 }
 
 export default Theme;
+
