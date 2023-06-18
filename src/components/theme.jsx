a9b113b7341f60074704ab58da2678ef7923d326
ralexrivero <ralexrivero@gmail.com>
2023-06-18 19:46:00 -0300

    fix theme buttons behavior

--- src/components/theme.jsx
+++ src/components/theme.jsx
@@ -1,22 +1,25 @@
 import React, { useState, useEffect } from 'react';
 
 const Theme = () => {
-  const [lightMode, setLightMode] = useState(true);
+  const [theme, setTheme] = useState('light');
 
-  const themeToggler = () => {
-    setLightMode(!lightMode);
+  const lightMode = () => {
+    setTheme('light');
   }
 
+  const darkMode = () => {
+    setTheme('dark')
+  }
   useEffect(() => {
     const body = document.querySelector('body');
-    body.classList.toggle('dark-theme', !lightMode);
-    body.classList.toggle('light-theme', lightMode);
+    body.classList.toggle('dark-theme', theme === 'dark');
+    body.classList.toggle('light-theme', theme === 'light');
   }, [lightMode]);
 
   return (
     <div className="theme">
-      <i onClick={themeToggler} className="theme-btn bi bi-brightness-high"></i>
-      <i onClick={themeToggler} className="theme-btn bi bi-moon"></i>
+      <i onClick={lightMode} className="theme-btn bi bi-brightness-high"></i>
+      <i onClick={darkMode} className="theme-btn bi bi-moon"></i>
     </div>
   );
 }
